fcf10be3812efa72caed43846127bbed47abb6c3
Radek-BFM <radekch@stanford.edu>
2020-09-16 00:50:57 -0700

    `autoCrop2Movies`

--- utilities/summary/mergeSummary.m
+++ utilities/summary/mergeSummary.m
@@ -1,9 +1,16 @@
 function [summaryOut] = mergeSummary(summary1,summary2)
 %CONCSUMMARY Concatenates, merges, two summary structures summary2 intro summary 1
+%SYNTAX
+% [summaryOut] = mergeSummary(summary1,summary2)
+
+%HISTORY
 % - 2020-09-13 20:40:52 -  by RC
 summaryOut = summary1;
 fNames = fieldnames(summary2);
 for iField=1:length(fNames)
+    if strcmpi(fNames{iField},'executionDuration')
+        continue % skipping time 
+    end
     summaryOut.(fNames{iField})=summary2.(fNames{iField});
 end
 

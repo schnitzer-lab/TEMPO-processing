a04c7d0ac5112befd24eb0218becfeabb6c4e920
Radek-BFM <radekch@stanford.edu>
2020-09-15 18:02:54 -0700

    moco refined diagnostic exporting mp4 movies

--- preprocessing/2_importMetadata/importLvMetaData.m
+++ preprocessing/2_importMetadata/importLvMetaData.m
@@ -1,8 +1,7 @@
-function [metadata]=importLvMetaData(lvSettinsPath,varargin)
+function [metadata]=importLvMetaData(lvSettingsFolder,varargin)
 % [metadata]=importLvMetaData(lvSettinsPath)
 % [metadata]=importLvMetaData(lvSettinsPath,'parameter',value,...)
-%
-%
+
 % HISTORY
 % - by SH 2020
 % - adopted for preprocessing by RC 06/23/2020
@@ -15,11 +14,13 @@ function [metadata]=importLvMetaData(lvSettinsPath,varargin)
 % - simplified and logically restricted to the content of the raw meta data
 % without conflating with processing.
 % - added depth detection
+% - 2020-09-15 16:44:48 - minimalistic - reading basic info fps, binning,
+% depth. If anything else we will add it
 
 
 %% OPTIONS
 
-[options]=MetadataStructure();
+% [options]=MetadataStructure(); removed by % - 2020-09-15 16:50:04 -   RC
 
 options.LVSettingsFile='allsettings.txt';
 options.autoCropping=true;
@@ -28,44 +29,27 @@ options.verbose=true;
 options.plot=true;
 options.guessBandPassFilter=true;
 
-%% VARIABLE CHECK
+% Variable check
 
 if nargin>=2
     options=getOptions(options,varargin);
 end
 
-
-%% CORE
-
-metadata=options;
-metadata.dataPath=allPaths.dcimgPath;
-metadata.totalBinning=metadata.softwareBinning*metadata.hardwareBinning;
-
-% Find Green & Red DCIMG files
-filename=dir(fullfile(allPaths.dcimgPath,'*.dcimg')); % G always comes before R
-% By default, Voltage is in Green Channel.
-[green, red]=filename.name;
-if ~strcmpi(metadata.voltageChannel,'green')
-    metadata.voltageChannel='Red'; % default for AcemNeon
-    metadata.referenceChannel='Green'; % default for mRuby3
-    [red, green]=filename.name;
-end
-metadata.voltageFileName=green;
-metadata.referenceFileName=red;
-
+% Core 
+metadata=struct;
 
 % Detect Sampling Rate
 try
-    A=fileread(fullfile(allPaths.dcimgPath,options.LVSettingsFile)); B=strfind(A, 'Actual frame rate');
-    metadata.fps=floor(str2double(A(B+153:B+157))); % in Hz
-    fprintf('Frame Rate: %1.0f Hz \n', metadata.fps);
+    A=fileread(fullfile(lvSettingsFolder,options.LVSettingsFile)); B=strfind(A, 'Actual frame rate');
+    metadata.fpsLV=floor(str2double(A(B+153:B+157))); % in Hz
+    fprintf('Frame Rate: %1.0f Hz \n', metadata.fpsLV);
 catch
     warning('Cannot detect sampling rate');
 end
 
 % Detect Hardware Binning
 try
-    A=fileread(fullfile(allPaths.dcimgPath,options.LVSettingsFile)); B=strfind(A, 'Binning (px)');
+    A=fileread(fullfile(lvSettingsFolder,options.LVSettingsFile)); B=strfind(A, 'Binning (px)');
     metadata.hardwareBinning=str2double(A(B+26:B+26));
     fprintf('Hardware Binning: %1.0f \n',metadata.hardwareBinning);
 catch
@@ -75,85 +59,23 @@ end
 % Detect depth of imaging RC added
 try
     strpattern='Depth (um)</Name>';
-    A=fileread(fullfile(allPaths.dcimgPath,options.LVSettingsFile)); B=strfind(A, strpattern);
+    A=fileread(fullfile(lvSettingsFolder,options.LVSettingsFile)); B=strfind(A, strpattern);
     metadata.depth=str2double(A(B+length(strpattern)+7:B+length(strpattern)+13));
     fprintf('Depth: %1.0f %sm \n',metadata.depth,char(181));
 catch
     warning('Cannot detect depth of imaging');
 end
 
-% Load TTL file if needed
-if metadata.loadTTL
-    try
-        Nametemp=dir('*_framestamps 0.txt');
-        [TTL]=importdata(Nametemp.name);
-        metadata.TTL=TTL(metadata.FramesRequested,4);
-    catch
-        warning('Cannot detect TTL file');
-    end
-end
 
 disps('Metadata succesfully loaded')
 
-    function disps(string) %overloading disp for this function - this function should be nested
-        %         temp=mfilename('fullpath');
-        %         [~,FUNCTION_NAME,~]=fileparts(temp);
-        FUNCTION_NAME='getRawMetaData';
+    function disps(string)
+        FUNCTION_NAME='importLvMetaData';
         if options.verbose
             fprintf('%s %s: %s\n', datetime('now'),FUNCTION_NAME,string);
         end
     end
 end
 
-function [metadata]=MetadataStructure()
-
-% Channels Settings
-metadata.dataPath=[];
-metadata.voltageChannel='Green'; % default for AcemNeon
-metadata.referenceChannel='Red'; % default for mRuby3
-metadata.voltageFileName=[];
-metadata.referenceFileName=[];
-
-% Acquisition Settings
-metadata.fps=[]; % in Hz
-metadata.frameDimension=[]; % in pixel
-metadata.totalFrames=[];
-metadata.dimension=[];
-metadata.depth=[];
-metadata.hardwareBinning=1; %usually
-
-% Loading Settings
-metadata.softwareBinning=8; % by default
-metadata.totalBinning=[];
-metadata.autoCropping=true;
-metadata.ROI=[]; % for autocroping; DCIMG and Matlab XY convention inverted
-metadata.vectorBandPassFilter=[];% to estime best filer for reg & moco
-
-% TTL for behavior sync
-metadata.loadTTL=false;
-metadata.TTL=[];
-
-% % Chunking option, all auto-determined
-% metadata.Chunking=false; % no chunking by default
-% metadata.ChunksNumber=[];
-% metadata.ChunksVector=[];
-% metadata.LoadedFramesperChunk=[]; % full or chunk window
-% metadata.DimensionLoadedperChunk=[];
-%
-% % Memory Settings
-% metadata.memoryAvailable=[];
-% metadata.FileSizeRaw=[];
-% metadata.FileSizeLoaded=[];
-% metadata.FileSizeLoadedperChunk=[];
-% metadata.FileSizeProcessed=[];
-
-% % Export settings
-% metadata.dcimgPath=[];
-% metadata.h5Path=[];
-% metadata.diagnosticLoading=[];
-% metadata.diagnosticRegistration=[];
-% metadata.diagnosticMotionCorr=[];
-% metadata.diagnosticUnmixing=[];
-end
 
 

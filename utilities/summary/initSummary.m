dbce20137f92fd291c30e6d54555f426d8d32510
Radek-BFM <radekch@stanford.edu>
2020-09-21 01:09:51 -0700

    `ttl2trails` debugged and refreshed

--- utilities/summary/initSummary.m
+++ utilities/summary/initSummary.m
@@ -1,15 +1,16 @@
-function [summary]=closeSummary()
-%
+function [summary]=initSummary(varargin)
 % HELP
 % Standard initialization of the summary within a function.
 % SYNTAX
-%[summary]= initSummary() - use 1 if no arguments are allowed
+%[summary]= initSummary()
+%[summary]= initSummary(inputOptions) 
 % - summary - %
 % OPTIONS:
 % - see below the section of code showing all possible input options and comments for their meaning. 
 
 % HISTORY
 % - 13-Sep-2020 20:04:52 - created by Radek Chrapkiewicz (radekch@stanford.edu)
+% - 2020-09-21 01:00:37 - you can pass input options to shorten function RC
 
 
 %% Summary preparation
@@ -26,6 +27,9 @@ summary.function=callingFunction;
 summary.executionStarted=datetime('now');
 summary.executionDuration=tic;
 
+if nargin>=1
+    summary.inputOptions=varargin{1};
+end
 
 
 

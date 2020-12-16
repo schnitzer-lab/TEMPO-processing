b4c61cde0ef86fbc18bca77f59ecaf227711e2e9
Radek-BFM <radekch@stanford.edu>
2020-12-15 23:16:22 -0800

    masking, reprocessing

--- utilities/h5/chunkh5.m
+++ utilities/h5/chunkh5.m
@@ -15,17 +15,12 @@ function [chunkSize,summary]=chunkh5(filepath,maxRAMfactor,varargin)
 % - summary - %
 % OPTIONS:
 % - see below the section of code showing all possible input options and comments for their meaning. 
-%
+
 % HISTORY
 % - 29-Jun-2020 21:41:54 - created by Radek Chrapkiewicz (radekch@stanford.edu)
-%
-% ISSUES
-% #1 - issue 1
-%
-% TODO
-% *1 - get the first working version of the function!
 
-%% OPTIONS (Biafra style, type 'help getOptions' for details) or: https://github.com/schnitzer-lab/VoltageImagingAnalysis/wiki/'options':-structure-with-function-configuration
+
+%% OPTIONS
 options=struct; % add your options below
 options.dataset='/mov';
 
@@ -34,13 +29,7 @@ options.dataset='/mov';
 if nargin>=3
 options=getOptions(options,varargin(1:end)); % CHECK IF NUMBER OF THE OPTION ARGUMENT OK!
 end
-input_options=options; % saving orginally passed options to output them in the original form for potential next use
-
-
-%% Summary preparation
-summary.function_path=mfilename('fullpath');
-summary.execution_started=datetime('now');
-summary.execution_duration=tic;
+summary=initSummary(options);
 
 
 %% CORE
@@ -49,12 +38,8 @@ summary.execution_duration=tic;
 
 summary.movie_size=msize;
 summary.availableRAM=checkRAM;
-
-
 summary.frame_MB=double(msize(1)*msize(2)*summarymov.bytes_per_px)/2^20;
-
 chunkSize=round(maxRAMfactor*summary.availableRAM/summary.frame_MB/2^20);
-
 summary.chunkSize=chunkSize;
 %% CLOSING
 summary.input_options=input_options; % passing input options separately so they can be used later to feed back to function input.

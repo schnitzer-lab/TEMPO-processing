8100936e5c01b1d00e55d4347d38db517f027064
unknown <oldbasilio@gmail.com>
2021-06-22 17:28:03 -0700

    extra specs saved as subfolder

--- packages/+rw/h5saveMovieSpecs.m
+++ packages/+rw/h5saveMovieSpecs.m
@@ -1,13 +1,16 @@
-function [info_cells] = h5saveMovieSpecs(h5filename, movie_specs)
+function [info_cells] = h5saveMovieSpecs(h5filename, movie_specs, specspath)
 % h5saveMovieSpecs - saves movie specs (inputed as MovieSpecs class object) to the h5 file
+    if(nargin < 3)
+        specspath = '/specs';
+    end
 
     [specs_cells, specs_names] = movie_specs.GetAllSpecs();
     
     info_cells = cell(length(specs_cells),1);
     
     for i = 1:length(specs_cells) 
-        info_cells{i+1} = ...
-            h5save(h5filename, specs_cells{i}, char(specs_names(i)));
+        %info_cells{i+1} = ...
+            rw.h5writeStruct(h5filename, specs_cells{i}, [specspath, '/', char(specs_names(i))]);
     end
 end
 

57aacd7a5dc81f2883f2c5e6662977b85e0265e9
unknown <oldbasilio@gmail.com>
2021-08-06 23:32:54 -0700

    somehow dct_readtimestamps got all messed up and failed to open anything; replaced with the one from radek's branch

--- utilities/dcimg/genTimestamps.m
+++ utilities/dcimg/genTimestamps.m
@@ -15,8 +15,11 @@ function [timestampPath]=genTimestamps(dcimgFilePath,varargin)
 
 % HISTORY
 % - 31-Aug-2020 12:49:14 - created by Radek Chrapkiewicz (radekch@stanford.edu)
+% - 2021-06-14 10:41:46 - fixing new dile name style with new time stamp exe program 06/21  RC
+
 options.reader_filename='dct_readtimestamps.exe';
-options.reader_folderpath=fileparts(mfilename('fullpath'));
+% options.reader_folderpath=fileparts(mfilename('fullpath'));
+options.reader_folderpath=fullfile(fileparts(fileparts(fileparts(mfilename('fullpath')))),'dct_readtimestamps'); % - 2021-06-14 11:06:26 -   RC
 
 if ~exist(options.reader_filename,'file')
     error('Cannot find %s, time stamp reader on the %s path!',options.reader_filename, options.reader_folderpath);
@@ -27,9 +30,12 @@ reader_path=fullfile(options.reader_folderpath,options.reader_filename);
 system(['"',reader_path,'"',' ',dcimgFilePath]);
 disps('File stamps generated')
 
+timestampPathNewName=[dcimgFilePath,'_0.txt']; % previously it was just '.txt' suffix % - 2021-06-14 10:42:50 -   RC
 timestampPath=[dcimgFilePath,'.txt'];
+if ~isfile(timestampPathNewName), error('time stamp not generated'); end
+movefile(timestampPathNewName,timestampPath); % - 2021-06-14 10:45:42 -   RC
 
-if exist(timestampPath,'file')
+if isfile(timestampPath)
     disps('Timestamps successfully created');
 else
     error('Timestamps file did not created');

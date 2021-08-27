605223f09cb615c3abd19c5d3ca0c41c8b5b25cb
unknown <oldbasilio@gmail.com>
2021-08-27 11:22:51 -0700

    option for adding just some frames from file added

--- packages/+rw/h5readMovie.m
+++ packages/+rw/h5readMovie.m
@@ -1,7 +1,20 @@
-function [movie, movie_specs] = h5readMovie(h5filename, dataset)
+function [movie, movie_specs] = h5readMovie(h5filename, varargin)
 %h5readMovie reads .h5 movie and associated specs
-    if(nargin < 2) dataset = '/mov'; end
+    
+    options = defaultOptions();
+    if(~isempty(varargin))
+        options = getOptions(options, varargin);
+    end
     
     movie_specs = rw.h5readMovieSpecs(h5filename);
-    movie = h5read(h5filename, dataset);
+    movie = h5read(h5filename, options.dataset, ...
+        [1, 1, options.frame_start], [Inf, Inf, options.frames_num]);
+end
+
+
+function options = defaultOptions()
+    
+    options.dataset = '/mov';
+    options.frame_start = 1;
+    options.frames_num = Inf;
 end

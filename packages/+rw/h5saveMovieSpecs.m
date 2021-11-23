c63c265e06780e41ef66b5de0aaf271b7db342cf
Vasily <OldBasilio@gmail.com>
2021-11-23 10:50:39 -0800

    rewrite functionality for specs

--- packages/+rw/h5saveMovieSpecs.m
+++ packages/+rw/h5saveMovieSpecs.m
@@ -1,18 +1,32 @@
-function [info_cells] = h5saveMovieSpecs(h5filename, movie_specs, specspath)
+function [info_cells] = h5saveMovieSpecs(h5filename, movie_specs, varargin)
 % h5saveMovieSpecs - saves movie specs (inputed as MovieSpecs class object) to the h5 file
-    if(nargin < 3)
-        specspath = '/specs';
+
+    options = defaultOptions();
+    if(~isempty(varargin))
+        options = getOptions(options, varargin);
     end
+
 
     [specs_cells, specs_names] = movie_specs.GetAllSpecs();
     
     info_cells = cell(length(specs_cells),1);
     
-    rw.h5writeStruct(h5filename, [0], [specspath, '/', 'extra_specs/empty_spec'])
+    dataset_extra =  [options.specspath, '/', 'extra_specs'];
+    if(~rw.h5checkDatasetExists(h5filename, dataset_extra))
+        rw.h5writeStruct(h5filename, [0], [dataset_extra, '/empty_spec'])
+    end
     
-    for i = 1:length(specs_cells) 
+    for i = 1:length(specs_cells)
+        dataset_spec = [options.specspath, '/', char(specs_names(i))];
+        if(~options.rewrite)
+            if(rw.h5checkDatasetExists(h5filename, dataset_spec)) continue; end
+        end
         %info_cells{i+1} = ...
-            rw.h5writeStruct(h5filename, specs_cells{i}, [specspath, '/', char(specs_names(i))]);
+            rw.h5writeStruct(h5filename, specs_cells{i}, dataset_spec);
     end
 end
 
+function options = defaultOptions()
+    options.specspath = '/specs';
+    options.rewrite = true;
+end

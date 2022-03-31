38ddb2b1b3935a14e3860f60d160841305064b19
Vasily <OldBasilio@gmail.com>
2022-03-31 14:26:05 -0700

    not the best way, but that's my folder, so all recent chenges go)

--- usersPipelines/Vasily/manual_processing/pipelines/pipeline_unmixing.m
+++ usersPipelines/Vasily/manual_processing/pipelines/pipeline_unmixing.m
@@ -1,38 +1,44 @@
 % clear
-addpath("../../../../_matlab_libs/deconvolution/")
+% addpath("../../../../_matlab_libs/deconvolution/")
 %%
-% 
-% basepath = "F:\GEVI_Wave\Preprocessed\Anesthesia\m46\20211217\meas00\";
-% postfix = "_bin16_reg_moco_cropMovie"; %_fr1-33000
+
+% basepath = "N:\GEVI_Wave_Monique\Preprocessed\Anesthesia\mAstr5299uk\20210919\meas01\";
+% postfix = "_cr_mc_down8s_moco_cropMovie"; %_fr1-33000
 % channels = ["G","R"];
 %%
 
 file1 = dir(fullfile(basepath, "/*" + "c" + channels(1) + postfix + ".h5"));
 file2 = dir(fullfile(basepath, "/*" + "c" + channels(2) + postfix + ".h5"));
 
-
 fullpathGin = fullfile(file1.folder, file1.name);
 fullpathRin = fullfile(file2.folder, file2.name);
 %%
 
-fullpath_maskManual = fullpathGin(1:(end-3)) + "_maskManual.bmp";
+% fullpath_maskManual = fullpathGin(1:(end-3)) + "_maskManual.bmp";
+% 
+% fullpathGm = movieApplyMask(fullpathGin, fullpath_maskManual);
+% fullpathRm = movieApplyMask(fullpathRin, fullpath_maskManual);
 
-fullpathGm = movieApplyMask(fullpathGin, fullpath_maskManual);
-fullpathRm = movieApplyMask(fullpathRin, fullpath_maskManual);
+fullpathGm = fullpathGin;
+fullpathRm = fullpathRin;
 %%
 % [[1, 0]; [0.0725, 1]];
-crosstalk_matrix =  [[1, 0]; [0.079, 1]]; %0.079 for newer ASAP3, 0.1187 for older ASAP2s with different filters %not sure this is correct, but it works for ASAP3 recordings
+% crosstalk_matrix =  [[1, 0]; [0, 1]];
+crosstalk_matrix =  [[1, 0]; [0.066, 1]]; 
+%i used to use 0.079 for newer ASAP3 but 0.65-0.68 seems to work much better; 
+%0.1 for older ASAP2s with different filters 
+%not sure this is correct, but it works for ASAP3 recordings
 delay = 0;
 [fullpathGd, fullpathRd] = moviesDecrosstalk(fullpathGm, fullpathRm, crosstalk_matrix, ...
     'framedelay', delay, 'skip', true);
-% fullpathGd = fullpathGin; fullpa thRd = fullpathRin;
+% fullpathGd = fullpathGin; fullpathRd = fullpathRin;
 %%
 
-% fullpathGbl = movieRemoveMean(fullpathGd, 'skip', true);
-% fullpathRbl = movieRemoveMean(fullpathRd, 'skip', true); 
+fullpathGbl = movieRemoveMean(fullpathGd, 'skip', true);
+fullpathRbl = movieRemoveMean(fullpathRd, 'skip', true); 
 
-fullpathGbl = movieExpBaselineCorrection(fullpathGd, 'skip', true); %movieRemoveMean(fullpathGd); %
-fullpathRbl = movieExpBaselineCorrection(fullpathRd, 'skip', true); %movieRemoveMean(fullpathRd); %
+% fullpathGbl = movieExpBaselineCorrection(fullpathGd, 'skip', true); %movieRemoveMean(fullpathGd); %
+% fullpathRbl = movieExpBaselineCorrection(fullpathRd, 'skip', true); %movieRemoveMean(fullpathRd); %
 
 %%
 
@@ -42,15 +48,14 @@ attn = 1e5;  rppl = 1e-2;
 [fullpathGhp, ~] =movieFilterExternalHighpass(...
     fullpathGbl, f0, wp,...
     'attn', attn, 'rppl', rppl, ...
-    'filtersdir', "F:\ConvolutionFilters\", ...
+    'filtersdir', "P:\GEVI_Wave\ConvolutionFilters\", ...
     'outdir', basepath, 'skip', true, ...
     'exepath', "C:\Users\Vasily\repos\VoltageImagingAnalysis\analysis\c_codes\compiled\hdf5_movie_convolution.exe");
-
 
 [fullpathRhp, ~] = movieFilterExternalHighpass(...
     fullpathRbl, f0, wp, ...
     'attn', attn, 'rppl', rppl, ...
-    'filtersdir', "F:\ConvolutionFilters\", ...
+    'filtersdir', "P:\GEVI_Wave\ConvolutionFilters\", ...
     'outdir', basepath, 'skip', true, ...
     'exepath', "C:\Users\Vasily\repos\VoltageImagingAnalysis\analysis\c_codes\compiled\hdf5_movie_convolution.exe");
 
@@ -61,25 +66,29 @@ attn = 1e5;  rppl = 1e-2;
 %%
 
 fullpathGhemo = ...
-    movieEstimateHemoGFilt(fullpathGhp, fullpathRhp, 'dt', 1.5, 'eps', .1, 'average_first', true,...
+    movieEstimateHemoGFilt(fullpathGhp, fullpathRhp, 'dt', 2, 'eps', 0.1, 'average_first', true,...
         'skip', true);%, 'reg_func', @(z,n) mean(z));
 %%
 
 fullpathGnohemo = movieRemoveHemoComponents(fullpathGhp, fullpathGhemo, 'divide', true);
-fullpathGnhDFF = movieDFF(fullpathGnohemo);
+% fullpathGnhDFF = movieDFF(fullpathGnohemo);
 % fullpathRfDFF = movieDFF(fullpathGhemo);
 %%
 %%
 
-delete(fullpathGm);
-delete(fullpathRm);
+if(~strcmp(fullpathGin, fullpathGm))
+    delete(fullpathGm);
+end
+if(~strcmp(fullpathRin, fullpathRm))
+    delete(fullpathRm);
+end
 delete(fullpathGd);
 delete(fullpathRd);
 
 % delete(fullpathGhp);
-delete(fullpathRhp);
+% delete(fullpathRhp);
 
-delete(fullpathGnohemo);
+% delete(fullpathGnohemo);
 %%
 
 % movieDownsample(fullpathGnhDFF, 1, 4)
@@ -99,6 +108,7 @@ df = 0.2;
 % movieMultitaperExternal(fullpathRfDFF, 'df', df, 'outdir', fullfile(basepath, "/PSD/"), ...
 %     'exepath', "C:\Users\Vasily\repos\VoltageImagingAnalysis\analysis\c_codes\compiled\hdf5_movie_fft.exe",...
 %     'f0plot', f0, 'tol',  1e-4, 'num_cores', 16, 'skip', true);
+% 
 % movieMultitaperExternal(fullpathGnhDFF, 'df', df, 'outdir', fullfile(basepath, "/PSD/"), ...
 %     'exepath', "C:\Users\Vasily\repos\VoltageImagingAnalysis\analysis\c_codes\compiled\hdf5_movie_fft.exe",...
 %     'f0plot', f0, 'tol',  1e-4, 'num_cores', 16, 'skip', true);

f430deedd94dcff06982beae89e96413a7455d3d
Vasily <OldBasilio@gmail.com>
2023-09-19 15:48:59 -0700

    some compatible modifications

--- analysis/allen/movieRegion2Trace.m
+++ analysis/allen/movieRegion2Trace.m
@@ -1,4 +1,4 @@
-function [trace, mask] = movieRegion2Trace(movie,contour,varargin)
+function [trace, mask] = movieRegion2Trace(movie,contours,varargin)
 %%
 
     options=defaultOptions(); % add your options below 
@@ -8,21 +8,27 @@ function [trace, mask] = movieRegion2Trace(movie,contour,varargin)
     end
     
     if(options.plot) imshow(std(movie,[],3), []); hold on; end
-    
-    touse = ~isnan(contour(:,2)) & ~isnan(contour(:,1));
-    
-    if(options.switchxy)
-        cx = contour(touse,2);
-        cy = contour(touse,1);
-    else
-        cx = contour(touse,1);
-        cy = contour(touse,2);       
-    end
+    if(~iscell(contours)) contours = {contours}; end
+
+    mask = zeros(size(movie,[1,2]));
+    for i_c = 1:length(contours)
+
+        contour = contours{i_c};
+        touse = ~isnan(contour(:,2)) & ~isnan(contour(:,1));
         
-    if(options.plot) plot(cx,cy); end
-    
-    mask = poly2mask(cx,cy, size(movie,1),size(movie,2));
-    
+        if(options.switchxy)
+            cx = contour(touse,2);
+            cy = contour(touse,1);
+        else
+            cx = contour(touse,1);
+            cy = contour(touse,2);       
+        end
+            
+        if(options.plot) plot(cx,cy); end
+        
+        mask = mask | poly2mask(cx,cy, size(movie,1),size(movie,2));
+    end
+
     trace=mask2trace(movie,mask)'; % - 2021-06-22 19:34:09 -   RC  
     
     if(options.plot) hold off; end

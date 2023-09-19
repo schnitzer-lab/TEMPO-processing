2806f834fb33879c54cc6568d34ca6df90e1e352
Vasily <OldBasilio@gmail.com>
2023-09-19 15:44:46 -0700

    updates to rw package

--- packages/+rw/h5getMeanTrace.m
+++ packages/+rw/h5getMeanTrace.m
@@ -6,23 +6,34 @@ function movie_mean = h5getMeanTrace(h5filename, varargin)
     end
 
     movie_size = rw.h5getDatasetSize(h5filename, options.dataset);
+    specs = rw.h5readMovieSpecs(h5filename);
+    nan_mask = double(specs.getMask()); nan_mask(nan_mask==0) = NaN;
 
     movie_total = zeros([movie_size(3), 1]);
     movie_current = [];
    
-    for i_c = 1:options.ncols_read:movie_size(2)
-        ncols_read = min(options.ncols_read, movie_size(2) - i_c+ 1);
+    for i_c = 1:options.nframes_read:movie_size(3)
+%         disp(i_c);
+        nframes_read = min(options.nframes_read, movie_size(3)-i_c+1);
 
-        movie_current = h5read(h5filename, options.dataset, [1, i_c, 1], [Inf, ncols_read, Inf]);
-        movie_total = movie_total + squeeze(sum(movie_current, [1,2], 'omitnan'));
+        movie_current = h5read(h5filename, options.dataset, [1, 1, i_c], [Inf, Inf, nframes_read]);
+
+        if(~isempty(nan_mask))
+            movie_current = movie_current.*nan_mask; %(:,i_c:(i_c+nrows_read-1));
+        end
+
+        movie_total(i_c:(i_c+nframes_read-1)) = sum(movie_current, [1,2], 'omitnan');
     end
 
-    movie_mean = movie_total / prod(movie_size(1:2));
+%     xy_size = prod(movie_size(1:2));
+%     if(~isempty(nan_mask)) xy_size = sum(~isnan(nan_mask), 'all'); end
+
+    movie_mean = movie_total;
 end
 
 
 function options = defaultOptions()
     
     options.dataset = '/mov';
-    options.ncols_read = Inf;
+    options.nframes_read = Inf;
 end

852c592c1439e23e1867277ba05affee4c35b4ec
Vasily <OldBasilio@gmail.com>
2024-05-30 22:02:01 -0700

    fixed poxtfixes for different binning

--- usersPipelines/Vasily/manual_processing/pipelines/pipeline_mass_DCIMGtoH5_preprocess_unmix.m
+++ usersPipelines/Vasily/manual_processing/pipelines/pipeline_mass_DCIMGtoH5_preprocess_unmix.m
@@ -1,20 +1,24 @@
 
 % clear;
-% close all;
+close all;
 %%
 
-% basefolder_raw = "\\Bfm\b\Michelle_temp\GEVI_Wave\Raw\";
-% files = dir(basefolder_raw + "\Visual\**\meas*");
-% recording_names = arrayfun(@(f) string(fullfile(f.folder, f.name)), files);
-% recording_names = erase(recording_names, basefolder_raw);
-%%
-recording_names =  ["Visual\m58\20220125\meas00", "Visual\m59\20220125\meas00"];
-%                     "\Visual\ms2103\20231021\meas"+arrayfun(@(k) string(num2str(k,'%02.f')), 0:1)];
-%     [rw.readlines("N:\GEVI_Wave\filelists\filelist_anesthesia_ace.txt")];
+basefolder_raw = "O:\michelle\V\GEVI_Wave\Raw\"; %"\\VoltageRaw\DCIMG\GEVI_Wave\Raw\"; 
+
+files = [dir(basefolder_raw + "\Visual\*mjr\2024*\meas*")]; %dir(basefolder_raw + "Visual\m40\20210824\meas00\");
+recording_names = arrayfun(@(f) string(fullfile(f.folder, f.name)), files);
+recording_names = erase(recording_names, basefolder_raw);
+
+
+
+%  recording_names = ...[rw.readlines("N:\GEVI_Wave\filelists\filelist_loco_canula.txt")]; 
+%  ["Visual\m40\20210824\meas00";];
+%  "\Visual\ms2103\20231021\meas"+arrayfun(@(k) string(num2str(k,'%02.f')), 0:1)];
+
 
 channels = ["G","R"];
 
-basefolder_raw = "\\VoltageRaw\DCIMG\GEVI_Wave\Raw\"; %"R:\GEVI_Wave\Raw\";% "M:\Raw Data Files\Raw\"; %
+% basefolder_raw = "\\VoltageRaw\DCIMG\GEVI_Wave\Raw\"; %"R:\GEVI_Wave\Raw\";% "M:\Raw Data Files\Raw\"; %%
 basefolder_converted = "S:\GEVI_Wave\Preprocessed\";
 basefolder_processing = "T:\GEVI_Wave\Preprocessed\";
 basefolder_preprocessed = "P:\GEVI_Wave\Preprocessed\";
@@ -26,9 +30,9 @@ unaccounted_hardware_binning = 1; %For old recordings, hardware binning is not a
 
 shifts0 = [0,0]; %[20,0]; % pix, between R and G channel due to cameras misalignment
 
-mouse_state = "awake";% "awake"; %"anesthesia"
+mouse_state = "awake";% "awake"; %"anesthesia" %"transition";
 crosstalk_matrix =  [[1, 0]; [0.07, 1]]; %[[1, 0]; [0.095, 1]]; %
-frame_range = [20, inf];
+frame_range = [50, inf];
 
 %%
 
@@ -43,8 +47,12 @@ for i_f = 1:length(recording_names)
     disp(string(i_f)+"/"+string(length(recording_names))+": "+recording_name);
     
     try
+        %%
+        
         channels = ["G","R"];
+%         skip_if_final_exists = true;
         pipeline_DCIMGtoH5
+        %%
     catch ME
         MEs_conv{length(MEs_conv)+1} = ME;
         warning(recording_name);
@@ -52,10 +60,13 @@ for i_f = 1:length(recording_names)
     end   
     
     try
+        %%
+        
         basefolder_output = "P:\GEVI_Wave\Preprocessed\";
-        postfix_in1 = "cG_bin8";
-        postfix_in2 = "cR_bin8";
+        postfix_in1 = "cG_bin"+string(binning);
+        postfix_in2 = "cR_bin"+string(binning);
         pipeline_preprocessing_2xmoco
+        %%
     catch ME
         MEs_conv{length(MEs_conv)+1} = ME;
         warning(recording_name);
@@ -63,10 +74,14 @@ for i_f = 1:length(recording_names)
     end   
 
     try
+        %%
+        
         basefolder_output = "N:\GEVI_Wave\Analysis\";    
-        postfix_in1 = "cG_bin8_mc";
-        postfix_in2 = "cR_bin8_mc_reg";
+        postfix_in1 = "cG_bin"+string(binning)+"_mc";
+        postfix_in2 = "cR_bin"+string(binning)+"_mc_reg";
+%         skip_if_final_exists = false;
         pipeline_unmixing
+        %%  
     catch ME
         MEs_conv{length(MEs_conv)+1} = ME;
         warning(recording_name);

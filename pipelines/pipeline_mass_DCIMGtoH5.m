16d11062fd3b85f04c1f5ccb9737c3bf5337d9de
Vasily <OldBasilio@gmail.com>
2025-07-14 15:57:22 -0700

    maxRAM increased

--- pipelines/pipeline_mass_DCIMGtoH5.m
+++ pipelines/pipeline_mass_DCIMGtoH5.m
@@ -8,18 +8,20 @@ diary(fullfile("P:\GEVI_Wave\Logs", ...
         strcat(string(datetime('now','Format','yyyyMMddHHmmss')),'_',mfilename(),'.log')));
 %%
 
-recording_names = ["Spontaneous\mDLRKlMORcre001\20240912\meas00", ...
-                   "Spontaneous\mRArchLKl001\20240912\meas00"];
+
+recording_names = ...
+    pathspattern("R:\GEVI_Wave\Raw\", "**\*\202505*\meas*", true)';
+
 %     [rw.readlines("N:\GEVI_Wave\filelists\filelist_anesthesia_ace.txt")];
 %%
 
-basefolder_raw = "\\Bfm\b\Hansol\Raw\"; %"R:\GEVI_Wave\Raw\";% 
-basefolder_converted = "\\Bfm\b\Hansol\Preprocessed\";
+basefolder_raw = "R:\GEVI_Wave\Raw\";% 
+basefolder_converted = "S:\GEVI_Wave\Preprocessed\";
 
 channels = ["G","R"];
 
 binning = 8;
-maxRAM = 0.1;
+maxRAM = 0.5;
 unaccounted_hardware_binning = 1; %For old recordings, hardware binning is not accounted for.
 %%
 

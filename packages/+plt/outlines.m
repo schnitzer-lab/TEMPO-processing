ef1ff84b6feed17cc2de4be2660ee2388202c42c
Vasily <OldBasilio@gmail.com>
2025-09-11 11:51:58 -0700

    refactoring

--- packages/+plt/outlines.m
+++ packages/+plt/outlines.m
@@ -5,16 +5,19 @@ function outlines(countors, x_lim, y_lim, varargin)
 
     tf = ishold();
     
-    for i_r = 1:size(countors, 3)
+    ncountors = size(countors, 3);
+    if(isempty(countors)), ncountors = 0; end
+
+    for i_r = 1:ncountors
         countors(countors(:,1,i_r) < x_lim(1), 1, i_r) = NaN;
         countors(countors(:,1,i_r) > x_lim(2), 1, i_r) = NaN;
         countors(countors(:,2,i_r) < y_lim(1), 1, i_r) = NaN;
         countors(countors(:,2,i_r) > y_lim(2), 1, i_r) = NaN;
     end
 
-    for i_r = 1:size(countors, 3)
+    for i_r = 1:ncountors
         plot(countors(:,1,i_r), countors(:,2,i_r), varargin{:}); hold on;
     end
-    if(~tf) hold off; end
+    if(~tf), hold off; end
 end
 

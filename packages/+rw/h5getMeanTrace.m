0192fce32fa85753376866ee8ec1b9aec0b090a8
Vasily <OldBasilio@gmail.com>
2025-12-19 12:59:46 -0800

    fixes for mask

--- packages/+rw/h5getMeanTrace.m
+++ packages/+rw/h5getMeanTrace.m
@@ -17,7 +17,7 @@ function [movie_mean, specs] = h5getMeanTrace(h5filename, varargin)
         movie_current = h5read(h5filename, options.dataset, [1, 1, i_c], [Inf, Inf, nframes_read]);
 
         if(~isempty(specs.getMaskNaN()) && options.mask)
-            movie_current = movie_current.*specs.getMaskNaN(); %(:,i_c:(i_c+nrows_read-1));
+            movie_current = movie_current.*specs.getMaskNaN(size(movie_current,[1,2])); %(:,i_c:(i_c+nrows_read-1));
         end
 
         movie_total(i_c:(i_c+nframes_read-1)) = mean(movie_current, [1,2], 'omitnan');

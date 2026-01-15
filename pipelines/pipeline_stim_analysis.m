92ee26373666090111ce720f2430a20bd69ae42f
Vasily <OldBasilio@gmail.com>
2026-01-14 16:03:11 -0800

    pipeline for quick stim checks + brain regions

--- pipelines/pipeline_stim_analysis.m
+++ pipelines/pipeline_stim_analysis.m
@@ -1,10 +1,12 @@
 
-basepath = "N:\GEVI_Wave\Analysis";
+basepath = "T:\GEVI_Wave\Analysis\Anesthesia\";
 
-files = dir(fullfile(basepath, "\Visual\mEndo*\**\meas*"));
+files = dir(fullfile(basepath, "\mv3101\20251210\meas1*"));
 recording_names = arrayfun(@(f) string(fullfile(f.folder, f.name)), files);
 recording_names = erase(recording_names, basepath);
 %%
+
+
 for i_f = 1:length(recording_names)    
     %%
     
@@ -12,25 +14,36 @@ for i_f = 1:length(recording_names)
     recording_name = recording_names(i_f);
     %%
 
-    fullpathGdFF = fullfile(basepath, recording_name, "cG_unmixed_dFF.h5");
+    fullpathGdFF = fullfile(basepath, recording_name, "cG_unmixedTR_dFF.h5");
     fullpathRdFF = fullfile(basepath, recording_name, "cR_dFF.h5");
     %%
+    
+    movieCopyReference(fullpathGdFF, [], 'folder_ref', "P:\GEVI_Wave\MiceAlignment\");
+    movieCopyReference(fullpathRdFF, [], 'folder_ref', "P:\GEVI_Wave\MiceAlignment\");
 
-    moviePlotTraceStim(fullpathGdFF, "mean");
-    moviePlotTraceStim(fullpathRdFF, "mean");
+    movieSavePreviewVideos(fullpathGdFF, 'skip', false, 'mask', true)
+    %%
+        
+    regions = {"RSP", "V1", [4,8,10,12,14,16,20]};
+    fullpats_regions = movieExtractRegionTrace(fullpathGdFF, regions);
     %%
 
-    fullpathGdFF = movieTrialAverage(fullpathGdFF);
-    fullpathRdFF = movieTrialAverage(fullpathRdFF);
+    movieMeanTraces(fullpats_regions, 'space', true);
     %%
 
-    nframes = rw.h5getDatasetSize(fullpathGdFF, '/mov', 3);
-    specs = rw.h5readMovieSpecs(fullpathGdFF);
-
+    moviePlotTraceStim(fullpats_regions(2), "mean");
+    %%
+    
+    fullpathG_trialav = movieTrialAverage(fullpathGdFF);
+    fullpathR_trialav = movieTrialAverage(fullpathRdFF);    
+    %%
+    
+    nframes = rw.h5getDatasetSize(fullpathG_trialav, '/mov', 3);
+    specs = rw.h5readMovieSpecs(fullpathG_trialav);
+    
     nseconds = floor(nframes/specs.getFps()*10)/10;
-    %%
-
-    moviesSavePreviewVideos([fullpathGdFF, fullpathRdFF], 'titles', ...
+    
+    moviesSavePreviewVideos([fullpathG_trialav, fullpathR_trialav], 'titles', ...
         ["G umx dFF", "R dFF"], 'nseconds', nseconds)
     %%
 end

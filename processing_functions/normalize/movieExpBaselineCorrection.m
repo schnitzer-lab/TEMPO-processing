88d3856d824435db57f45af3bff00ceb16b0dec4
Vasily <OldBasilio@gmail.com>
2026-02-18 20:40:13 -0800

    checks for successful fit

--- processing_functions/normalize/movieExpBaselineCorrection.m
+++ processing_functions/normalize/movieExpBaselineCorrection.m
@@ -30,47 +30,40 @@ function fullpath_out = movieExpBaselineCorrection(fullpath_movie, varargin)
     disp("movieExpBaselineCorrection: reading movie")
     
     [Min, specs] = rw.h5readMovie(fullpath_movie);
-
     %%
     
     disp("movieExpBaselineCorrection: fitting for the mean trace")
     ts = (1:size(Min, 3))';
-
-    nan_mask = ones(size(Min, [1,2]));
-    if(~isempty(specs.getMask())) nan_mask(specs.getMask() == 0) = NaN; end
-
-    meanmovie = double(squeeze(mean(Min.*nan_mask, [1,2], 'omitnan')));
-
+    if(~isempty(specs.getMask()))
+        m = squeeze(mean(Min.*specs.getMaskNaN(), [1,2], 'omitnan'));
+    else
+        m = squeeze(mean(Min, [1,2], 'omitnan'));
+    end
     %%
 
     baseline = @(a, b1, c1, b2, c2, x) (a + c1.*exp(-x./b1) + c2.*exp(-x./b2));  
     
     fo = fitoptions('Method','NonlinearLeastSquares',...
-                    'StartPoint', [min(meanmovie)/2, 20*specs.getFps(), min(meanmovie)/2, 200*specs.getFps(), 0],...
+                    'StartPoint', [mean(m), 20*specs.getFps(), 0, 200*specs.getFps(), 0],...
                     'lower',[0 options.tmin*specs.getFps() 0 options.tmin*specs.getFps() 0], ...
-                    'upper',[Inf Inf Inf Inf Inf]);
+                    'upper',[Inf length(m)/2 Inf Inf Inf]);
 
-    f0=fit(ts, meanmovie, baseline, fo);
+    f0=fit(ts, double(m), baseline, fo);
     %%
 
     fo1 = copy(fo);
-    fo1.Upper(5) = fo1.Lower(5);
-    fo1.Upper(4) = fo1.Lower(4);
-    f01=fit(ts, meanmovie, baseline, fo1);
+    fo1.Upper(5) = fo1.Lower(5); fo1.Upper(4) = fo1.Lower(4);
+    f01=fit(ts, double(m), baseline, fo1);
     
     useC2 = true;
-    % if improvement is less that 5%, don't use free term c2
-    if(norm(f01(ts)-meanmovie)/norm(f0(ts)-meanmovie)-1 < options.twoexpimpr )
-        useC2 = false;
-        f0 = f01;     
+    % if improvement is less, don't use free term c2
+    if(norm(f01(ts)-m)/norm(f0(ts)-m)-1 < options.twoexpimpr )
+        useC2 = false; f0 = f01;     
     end
-        
-    ga = f0.a/min(meanmovie);
     %%
 
     fig_meanfit = plt.getFigureByName("movieExpBaselineCorrection - mean trace");
-    plot(f0, ts, meanmovie, '-'); 
-    xlabel("frame"); 
+    plot(f0, ts, m, '-'); xlabel("frame #"); 
     text(0.15*mean(ts), max(get(gca, 'YLim')) - 0.2*diff(get(gca, 'YLim')), evalc('f0'))
     drawnow;
     %%
@@ -80,46 +73,34 @@ function fullpath_out = movieExpBaselineCorrection(fullpath_movie, varargin)
     nx = size(Min, 1); ny = size(Min, 2);
     
     A = nan([nx*ny, 1]);
-    B1 = nan([nx*ny, 1]);
-    C1 = nan([nx*ny, 1]);
-    B2 = nan([nx*ny, 1]);
-    C2 = nan([nx*ny, 1]);
-    
-    Min = reshape(Min, [nx*ny, size(Min, 3)]);
+    B1 = nan([nx*ny, 1]); C1 = nan([nx*ny, 1]);
+    B2 = nan([nx*ny, 1]); C2 = nan([nx*ny, 1]);
     %%
     
-    X1 = f0.a*ones(size(Min,2),1);
+    X1 = f0.a*ones(length(ts),1);
     X2 = f0.c1*exp(-ts/f0.b1);
     X3 = f0.c2*exp(-ts/f0.b2);
-    nan_pos = isnan(mean(Min,2));
     %%
 
-    if(useC2)
-        ft = [X1, X2, X3]\Min(~nan_pos, :)';
-%         ft(:, ipix) = lsqnonneg(X,  double(Min(ipix,:))');
-    else 
-        ft = [X1, X2]\Min(~nan_pos, :)';
-%         ft(:, ipix) = [lsqnonneg(X,  double(Min(ipix,:))'); 0];
+    Min = reshape(Min, [nx*ny, size(Min, 3)]);
+    
+    if(useC2), X = [X1, X2, X3]; else, X = [X1,X2]; end
+    ft = NaN(size(X,2), size(Min,1));
+    parfor ipix = 1:size(Min,1)
+        if(any(isnan(Min(ipix,:)))), continue; end
+        ft(:, ipix) = lsqnonneg(X,  double(Min(ipix,:))');
     end
     %%
 
-    A(~nan_pos) = ft(1,:)*f0.a;
-    B1(~nan_pos) = f0.b1; 
-    C1(~nan_pos) = ft(2,:)*f0.c1;
-
-    B2(~nan_pos) = f0.b2; 
-    C2(~nan_pos) = f0.c2;
-    if(useC2) 
-        C2(~nan_pos) = ft(3,:)*f0.c2;
-    end 
-    C2(C2<0) = 0;
+    A(:)= ft(1,:)*f0.a;
+    B1(:) = f0.b1; C1(:) = ft(2,:)*f0.c1;
+    B2(:) = f0.b2; C2(:) = f0.c2;
+    if(useC2), C2(:) = ft(3,:)*f0.c2; end 
     %%
     
     A = reshape(A, [nx,ny]);
-    B1 = reshape(B1, [nx,ny]);
-    C1 = reshape(C1, [nx,ny]);
-    B2 = reshape(B2, [nx,ny]);
-    C2 = reshape(C2, [nx,ny]);
+    B1 = reshape(B1, [nx,ny]); C1 = reshape(C1, [nx,ny]);
+    B2 = reshape(B2, [nx,ny]); C2 = reshape(C2, [nx,ny]);
     
     Min = reshape(Min, [nx,ny,size(Min,2)]);
     %%
@@ -133,15 +114,13 @@ function fullpath_out = movieExpBaselineCorrection(fullpath_movie, varargin)
     subplot(2,3,5); histogram(plt.saturate(C1,[0,0.99])); 
     subplot(2,3,6); histogram(plt.saturate(C2,[0,0.99])); 
     
-    sgtitle(["M = (A + C_1exp(-t/b_1) + C_2exp(-t/b_2)), " + ...
-             sprintf("b_1=%.1fs, b_2=%.1fs", f0.b1/specs.getFps(), f0.b2/specs.getFps())])
+    sgtitle("M = (A + C_1exp(-t/b_1) + C_2exp(-t/b_2)), " + ...
+            sprintf("b_1=%.1fs, b_2=%.1fs", f0.b1/specs.getFps(), f0.b2/specs.getFps()) )
     %%
     
     Mbl = (baseline(A, B1, C1, B2, C2, permute(ts,[3,2,1])));
     
-    % correction to the final value in recording not an ideal soution, 
-    % but needed for the cases when the exponent fitted has huge
-    % decay time and overcorrects
+    % correction to the final value in recording
     if(options.divide)
         Mout = Min./(Mbl./Mbl(:,:,end));
     else
@@ -149,13 +128,21 @@ function fullpath_out = movieExpBaselineCorrection(fullpath_movie, varargin)
     end
     %%
 
+    mout = squeeze(mean(Mout, [1,2], 'omitnan'));
+    bl = squeeze(mean(Mbl, [1,2], 'omitnan'));
+    
+    r2 = norm(m-bl) / norm(m);
+    a =  median(m-bl) / std(m-bl);
+
     fig_meanout = plt.getFigureByName("movieExpBaselineCorrection - traces");
-    plt.tracesComparison([...
-        squeeze(mean(Min, [1,2], 'omitnan')), ...
-        squeeze(mean(Mout, [1,2], 'omitnan')), ...
-        squeeze(mean(Mbl, [1,2], 'omitnan'))], ...
+    plt.tracesComparison([m, mout, bl], ...
         'fps', specs.getFps(), 'fw', 0.1, 'labels', ["raw", "baseline-corrected", "baseline"], ...
-        'nomean', false)
+        'nomean', false, 'spectra', false)
+    title(sprintf("baseline correction (rs=%.2f, a=%.1f)", r2, a))
+    %%
+    
+    if(r2 > options.r2_thresh), error('bad fit - fit difference'); end
+    if(abs(a) > options.a_thresh), error('bad fit - fit asymmetry'); end
     %%
     
     disp("movieExpBaselineCorrection: saving")
@@ -182,9 +169,12 @@ function options = defaultOptions(basepath)
     options.outdir = basepath;
 
     options.tmin = 2; %s
-    options.twoexpimpr = 5e-2; % use 2-exp model if there is at least 5% improvement
+    options.twoexpimpr = 0; % use 2-exp model if there is relative improvement more than twoexpimpr
+    options.r2_thresh = 0.05;
+    options.a_thresh = 1; 
+
     options.divide = false;
 
-      options.skip = true;
+    options.skip = true;
 end
 %%

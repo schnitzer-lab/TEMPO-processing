087888b4ee490934f5e5a8c477394f64893ce1ea
Vasily <OldBasilio@gmail.com>
2026-02-19 11:29:22 -0800

    multiple outlines plotting

--- packages/+plt/outlines.m
+++ packages/+plt/outlines.m
@@ -1,22 +1,24 @@
-function outlines(countors, x_lim, y_lim, varargin)
+function outlines(contours, x_lim, y_lim, varargin)
     
-    if(nargin < 2 || isempty(x_lim)) x_lim = [-Inf,Inf]; end
-    if(nargin < 3 || isempty(y_lim)) y_lim = [-Inf,Inf]; end 
+    if(nargin < 2 || isempty(x_lim)), x_lim = [-Inf,Inf]; end
+    if(nargin < 3 || isempty(y_lim)), y_lim = [-Inf,Inf]; end 
 
     tf = ishold();
     
-    ncountors = size(countors, 3);
-    if(isempty(countors)), ncountors = 0; end
-
-    for i_r = 1:ncountors
-        countors(countors(:,1,i_r) < x_lim(1), 1, i_r) = NaN;
-        countors(countors(:,1,i_r) > x_lim(2), 1, i_r) = NaN;
-        countors(countors(:,2,i_r) < y_lim(1), 1, i_r) = NaN;
-        countors(countors(:,2,i_r) > y_lim(2), 1, i_r) = NaN;
+    if(~iscell(contours) && ~isempty(contours))
+        contours = squeeze(mat2cell(contours, ...
+            size(contours,1), size(contours,2), ones([size(contours,3),1])));
     end
 
-    for i_r = 1:ncountors
-        plot(countors(:,1,i_r), countors(:,2,i_r), varargin{:}); hold on;
+    for i_r = 1:length(contours)
+        contours{i_r}(contours{i_r}(:,1) < x_lim(1), 1) = NaN;
+        contours{i_r}(contours{i_r}(:,1) > x_lim(2), 1) = NaN;
+        contours{i_r}(contours{i_r}(:,2) < y_lim(1), 2) = NaN;
+        contours{i_r}(contours{i_r}(:,2) > y_lim(2), 2) = NaN;
+    end
+
+    for i_r = 1:length(contours)
+        plot(contours{i_r}(:,1), contours{i_r}(:,2), varargin{:}); hold on;
     end
     if(~tf), hold off; end
 end

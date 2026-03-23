51d7f596ec31e9b60e4369e6582aa398ed1c8fad
Vasily <OldBasilio@gmail.com>
2026-03-22 17:30:59 -0700

    slightly better logic for frames extraction

--- processing_functions/manipulateST/movieExtractFrames.m
+++ processing_functions/manipulateST/movieExtractFrames.m
@@ -1,6 +1,6 @@
-function fullpath_out = movieExtractFrames(fullpath, frames_range, varargin)
+function fullpath_out = movieExtractFrames(fullpath_in, frames_range, varargin)
     
-    [basepath, basefilename, ext, postfix] = filenameSplit(fullpath, '_');
+    [basepath, filename, ext] = fileparts(fullpath_in);
 
     options = defaultOptions(basepath);
     if(~isempty(varargin))
@@ -10,10 +10,9 @@ function fullpath_out = movieExtractFrames(fullpath, frames_range, varargin)
     postfix_new = "_fr" + num2str(frames_range(1)) + "-" + num2str(frames_range(2));
     %%
     
-    if (~isfolder(options.outdir)) mkdir(options.outdir); end
+    if (~isfolder(options.outdir)), mkdir(options.outdir); end
 
-    filename_out = basefilename + postfix + postfix_new;
-    fullpath_out = fullfile(options.outdir, filename_out + ext);
+    fullpath_out = fullfile(options.outdir, filename + postfix_new + ext);
     
     if (isfile(fullpath_out))
         if(options.skip)
@@ -26,7 +25,7 @@ function fullpath_out = movieExtractFrames(fullpath, frames_range, varargin)
     end
     %%
 
-    nframes = rw.h5getDatasetSize(fullpath, '/mov', 3);
+    nframes = rw.h5getDatasetSize(fullpath_in, '/mov', 3);
     %%
     
     if(frames_range(2) == Inf )
@@ -38,14 +37,20 @@ function fullpath_out = movieExtractFrames(fullpath, frames_range, varargin)
     if(frames_range(2) <= 0)
        frames_range(2) =  nframes + frames_range(2);
     end
+
     if(frames_range(1) == 1 && frames_range(2) == nframes)
-        fullpath_out = fullpath;
+        if(options.outdir ~= fileparts(fullpath_in))
+            fullpath_out = fullfile(options.outdir, filename + ext);
+            copyfile(fullpath_in, fullpath_out);
+        else 
+            fullpath_out = fullpath_in;            
+        end
         return;
     end
     %%
     
     displog("reading movie")
-    [M, specs] = rw.h5readMovie(fullpath, ...
+    [M, specs] = rw.h5readMovie(fullpath_in, ...
             'frames_num', frames_range(2) - frames_range(1) + 1, ...
             'frame_start', frames_range(1));
     %%
@@ -54,7 +59,7 @@ function fullpath_out = movieExtractFrames(fullpath, frames_range, varargin)
     %%
 
     displog("saving")
-    rw.h5saveMovie( fullpath_out, M,  specs); 
+    rw.h5saveMovie(fullpath_out, M,  specs); 
     
 %     saveas(fig_trace, fullfile(options.processingdir, filename_out + "_traces.png"))
 %     saveas(fig_trace, fullfile(options.processingdir, filename_out + "_traces.fig"))
@@ -63,6 +68,8 @@ end
 
 function options = defaultOptions(basepath)
     
+    options.outdir = basepath;
+
     options.outdir = basepath;
     options.skip = true;
 end

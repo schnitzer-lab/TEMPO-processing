94b54c870b44bdd73613f0ea76ee276072468c55
Vasily <OldBasilio@gmail.com>
2026-07-13 15:06:08 -0700

    better plotting

--- processing_functions/regions/movieRegion2Trace.m
+++ processing_functions/regions/movieRegion2Trace.m
@@ -7,13 +7,15 @@ function [trace, mask] = movieRegion2Trace(movie,contours,varargin)
         options=getOptions(options,varargin(1:end)); % CHECK IF NUMBER OF THE OPTION ARGUMENT OK!
     end
     
-    if(isempty(options.m2d)) options.m2d = std(movie,[],3); end
+    if(isempty(options.m2d)), options.m2d = std(movie,[],3); end
     
     if(options.plot) 
-        im = imshow(options.m2d, []); hold on; 
+        im = imagesc(options.m2d); hold on; 
+        axis equal; axis off;
+        colormap('gray');
         set(im, 'AlphaData', ~isnan(options.m2d) );
     end
-    if(~iscell(contours)) contours = {contours}; end
+    if(~iscell(contours)), contours = {contours}; end
 
     mask = zeros(size(movie,[1,2]));
     for i_c = 1:length(contours)
@@ -29,19 +31,23 @@ function [trace, mask] = movieRegion2Trace(movie,contours,varargin)
             cy = contour(touse,2);       
         end
             
-        if(options.plot) plot(cx,cy); end
+        if(options.plot), 
+            plot(cx,cy, 'Color', options.plotcolor, 'LineWidth', 1.5); 
+            drawnow; 
+        end
         
         mask = mask | poly2mask(cx,cy, size(movie,1),size(movie,2));
     end
 
     trace=mask2trace(movie,mask)'; % - 2021-06-22 19:34:09 -   RC  
     
-    if(options.plot) hold off; end
+    if(options.plot), hold off; end
 
 end  
 
 function options = defaultOptions()
     options.switchxy = true;
     options.plot = false;
+    options.plotcolor = [0,1,0];
     options.m2d = [];
 end
